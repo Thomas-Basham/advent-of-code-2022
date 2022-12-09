5f9ec19d5562cfcda69d058c778d0b6138b16bbd
Thomas Basham <Bashamtg@gmail.com>
2022-12-09 09:59:13 -0800

    TODO: finish part 2

--- solutions/day_8.py
+++ solutions/day_8.py
@@ -1,6 +1,6 @@
-# https://adventofcode.com/2022/day/1
+# https://adventofcode.com/2022/day/8
 import time
-import re
+import numpy
 
 day = "8"
 inputs = ""
@@ -16,20 +16,20 @@ def convert_test_input(case):
   print("INPUTS:", inputs)
 
 
+def CheckForLess(list1, val):
+  # traverse in the list
+  for x in list1:
+    # compare with all the
+    if x >= val:
+      return False
+
+  return True
+
+
 def solution():
-  def CheckForLess(list1, val):
-
-    # traverse in the list
-    for x in list1:
-      # compare with all the
-      if x >= val:
-        return False
-
-    return True
-
   def check_col(col_index, row_index):
     collumn = [inputs[x][col_index] for x in range(len(inputs))]
-    print("CHECKING COLLUMN:", collumn)
+    print("CHECKING COLLUMN:", collumn, "MADE FROM col, rowL", col_index, row_index)
 
     # check the top half of collumn
     top_half = collumn[:row_index]
@@ -41,7 +41,7 @@ def solution():
     # print("CHECK BOTTOM:", CheckForLess(bot_half, collumn[row_index]))
 
     if CheckForLess(top_half, collumn[row_index]) or CheckForLess(bot_half, collumn[row_index]):
-      print("VISIBLE!")
+      print("COL VISIBLE!")
       return True
 
   def check_row(col_index, row_index):
@@ -49,6 +49,7 @@ def solution():
     print("CHECKING ROW:", row, "MADE FROM col ,row:", col_index, row_index)
     left_half = row[:col_index]
     right_half = row[col_index + 1:]
+    print("left_half, right_half", left_half, right_half)
 
     if CheckForLess(left_half, row[col_index]) or CheckForLess(right_half, row[col_index]):
       print("ROW VISIBLE")
@@ -75,23 +76,87 @@ def solution():
           if check_row(ind, index) or check_col(ind, index):  # check up and down
             trees_visible += 1
 
-  print(trees_visible)
-
   end_time = time.time()
   print(f"It took {end_time - start_time:.2f} seconds to compute")
+
+  print(trees_visible)
+  return trees_visible
 
 
 def solution_part_2():
+  trees_visible = 0
+
+  def check_col(col_index, row_index):
+    col_obj = {"top": 1, "bot": 1}
+    collumn = [inputs[x][col_index] for x in range(len(inputs))]
+    print("CHECKING COLLUMN:", collumn, "MADE FROM col, rowL", col_index, row_index)
+
+    # check the top half of collumn
+    top_half = collumn[:row_index]
+    # check the bottom half of collumn
+    bot_half = collumn[row_index + 1:]
+    print("top_half, bot_half", top_half, bot_half)
+
+    # print("CHECK TOP:", CheckForLess(top_half, collumn[row_index]))
+    # print("CHECK BOTTOM:", CheckForLess(bot_half, collumn[row_index]))
+
+    if CheckForLess(top_half, collumn[row_index]):
+      print("COL VISIBLE!")
+      col_obj['top'] += len(top_half) - 1
+      # return True, len(top_half)
+    if CheckForLess(bot_half, collumn[row_index]):
+      print("COL VISIBLE!")
+      col_obj['bot'] += len(bot_half)
+      # return True, len(bot_half)
+    return col_obj
+
+  def check_row(col_index, row_index):
+    row_obj = {"left": 1, "right": 1}
+
+    row = inputs[row_index]
+    print("CHECKING ROW:", row, "MADE FROM col ,row:", col_index, row_index)
+    left_half = row[:col_index]
+    right_half = row[col_index + 1:]
+    print("left_half, right_half", left_half, right_half)
+
+    if CheckForLess(left_half, row[col_index]):
+      print("ROW VISIBLE")
+      row_obj["left"] += len(left_half) - 1
+      # return True, len(left_half)
+    if CheckForLess(right_half, row[col_index]):
+      print("ROW VISIBLE")
+      row_obj["right"] += len(left_half) - 1
+
+    return row_obj
+
+    # return True, len(right_half)
+
+  # DRIVER
+  for index, row in enumerate(inputs):
+    if index == 0 or index == len(inputs) - 1:
+      pass
+    else:
+      for ind, col in enumerate(row):
+        temp_nums = []
+        if ind != 0 and ind != len(row) - 1:
+          # if check_row(ind, index):
+          #   pass
+          print(check_col(ind, index))
+          # if check_col(ind, index):  # check up and down
+
   end_time = time.time()
   print(f"It took {end_time - start_time:.2f} seconds to compute")
+
+  print(trees_visible)
+  return trees_visible
 
 
 if __name__ == '__main__':
   # test input
-  # convert_test_input(f'inputs/day_{day}_test.txt')
+  convert_test_input(f'inputs/day_{day}_test.txt')
 
   ## challenge input
-  convert_test_input(f'inputs/day_{day}.txt')
+  # convert_test_input(f'inputs/day_{day}.txt')
 
-  solution()
-  # solution_part_2()
+  # solution()
+  solution_part_2()
