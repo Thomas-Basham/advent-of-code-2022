603deee180b96ca1f082cd6873064daddcb80b6e
Thomas Basham <Bashamtg@gmail.com>
2022-12-12 10:33:48 -0800

    day 10 part 2 solved

--- solutions/day_10.py
+++ solutions/day_10.py
@@ -1,18 +1,28 @@
 # https://adventofcode.com/2022/day/10
 import time
-import numpy as np
+import math
 
 day = "10"
 inputs = ""
-
+total_cycles = 0
 start_time = time.time()
 
 
 def convert_test_input(case):
   global inputs
+  global total_cycles
   with open(case) as f:
     for line in f.readlines():
       inputs += line
+      command = line[:4]
+      if command.startswith("addx"):
+        # print("COMMAND: ", command, ammount)
+        total_cycles += 2
+      if command.startswith('noop'):
+        # print("COMMAND: ", command)
+        total_cycles += 1
+
+  print("total_cycles", total_cycles)
   inputs = inputs.splitlines()
 
   print("INPUTS:", inputs)
@@ -45,6 +55,34 @@ def solution():
 
 
 def solution_part_2():
+  rows = [["." for _ in range(40)] for _ in range(int(total_cycles / 40))]
+  cycle = 0
+  x = 0
+
+  for i in inputs:
+    command = i[:4]
+    amount = i[5:]
+
+    row = math.floor(cycle / 40)
+    row_i = cycle - (row * 40)
+
+    if row_i in range(x, x + 3):
+      rows[row][row_i] = '#'
+
+    cycle += 1
+    row_i += 1
+
+    if command.startswith("addx"):
+      if row_i in range(x, x + 3):
+        rows[row][row_i] = '#'
+      cycle += 1
+      row_i += 1
+
+      x += int(amount)
+
+  for i in rows:
+    print(i)
+
   end_time = time.time()
   print(f"It took {end_time - start_time:.2f} seconds to compute")
 
@@ -57,4 +95,5 @@ if __name__ == '__main__':
   convert_test_input(f'inputs/day_{day}.txt')
 
   solution()
-  # solution_part_2()
+  solution_part_2()
+  # PZULBAUA
