e5631749ef60af923ab3c3db326cc00353ecbffc
Thomas Basham <Bashamtg@gmail.com>
2022-12-12 18:21:30 -0800

    TODO: solve part 2'

--- solutions/day_11.py
+++ solutions/day_11.py
@@ -17,17 +17,18 @@ def convert_test_input(case):
     for line in f.readlines():
       inputs += line
   inputs = inputs.splitlines()
-  print("INPUTS:", inputs)
+  # print("INPUTS:", inputs)
   monkeys = [inputs[i:i + 6] for i in range(0, len(inputs), 7)]
   for m in monkeys:
-    m[1] = re.findall(r'[0-9]+', m[1])
+    m[1] = list(map(int, re.findall(r'[0-9]+', m[1])))
     m[2] = m[2].strip("  Operation: new ").strip('= ')
     m.append(0)
     del m[0]
+  print(monkeys)
 
 
 def solution():
-  global throw_to, operation, commands
+  # global throw_to, operation, commands
   for _ in range(20):
     for monkey in monkeys:
       worry_levels = monkey[0]
@@ -40,7 +41,7 @@ def solution():
           for c in range(len(commands)):
             if commands[c] == 'old ' or commands[c] == 'old':
               commands[c] = num
-          operation = int(commands[0]) * int(commands[1])
+          operation = commands[0] * commands[1]
 
         if '+' in monkey[1]:
           commands = monkey[1].split('+ ')
@@ -49,7 +50,7 @@ def solution():
             if commands[c] == 'old ' or commands[c] == 'old':
               commands[c] = num
 
-          operation = int(commands[0]) + int(commands[1])
+          operation = commands[0] + commands[1]
 
         operation = operation / 3
 
@@ -78,8 +79,56 @@ def solution():
 
 
 def solution_part_2():
+  # global throw_to, operation, commands
+  global operation, throw_to
+  for i in range(1000):
+    for monkey in monkeys:
+      worry_levels = monkey[0]
+      thrown_counter = len(worry_levels)
+
+      # while thrown_counter > 0
+      for num in worry_levels:
+
+        if '*' in monkey[1]:
+          commands = monkey[1].split('* ')
+          for c in range(len(commands)):
+            if commands[c] == 'old ' or commands[c] == 'old':
+              commands[c] = num
+          operation = math.floor(int(commands[0]) * int(commands[1]))
+
+        if '+' in monkey[1]:
+          commands = monkey[1].split('+ ')
+          for c in range(len(commands)):
+            if commands[c] == 'old ' or commands[c] == 'old':
+              commands[c] = num
+          operation = math.floor(int(commands[0]) + int(commands[1]))
+
+        test_amnt = int(re.findall(r'[0-9]+', monkey[2])[0])
+        test = operation % test_amnt == 0
+
+        if test == True:
+          throw_to = int(re.findall(r'[0-9]+', monkey[3])[0])
+        if test == False:
+          throw_to = int(re.findall(r'[0-9]+', monkey[4])[0])
+
+        # operation = operation / 3 # NEED TO FIGURE OUT HOW TO WORK THIS BACK IN
+        monkeys[throw_to][0].append(operation)
+
+      monkey[-1] += thrown_counter
+      # for _ in range(thrown_counter):
+      #   monkey[0].pop(0)
+
+      [monkey[0].pop(0) for _ in range(thrown_counter)]
+    print(i)
+
+  # print(monkeys)
+  inspection_count_list = sorted([i[-1] for i in monkeys])
+  monkey_business = inspection_count_list[-1] * inspection_count_list[-2]
   end_time = time.time()
   print(f"It took {end_time - start_time:.2f} seconds to compute")
+
+  print(monkey_business)
+  return monkey_business
 
 
 if __name__ == '__main__':
@@ -89,5 +138,5 @@ if __name__ == '__main__':
   ## challenge input
   convert_test_input(f'inputs/day_{day}.txt')
 
-  solution()
-  # solution_part_2()
+  # solution()
+  solution_part_2()
